cbb88fd2a4c66a9a6c0301a2b61132c3522b4f92
dmitriy <b27011971@inbox.ru>
2023-05-15 11:44:12 +0300

    add JavaSyntax task0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 5;
+        int age = 7;
+        String documents = "Гражданин";
+        String behavior = "Преступник";
+    }
+}
