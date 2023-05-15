f46839cd591f537e9151d689ccfd343ad8387f84
dmitriy <b27011971@inbox.ru>
2023-05-15 10:55:47 +0300

    add JavaSyntax task0102

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0102/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task01.task0102;
+
+/* 
+Включить аварийное питание и ручное управление
+*/
+
+import java.sql.SQLOutput;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Аварийное питание включено");
+        System.out.println("Ручное управление включено");
+    }
+}
