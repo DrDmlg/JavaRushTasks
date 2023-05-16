b515d046d9d94b4477592e2a263da0672ea40c43
dmitriy <b27011971@inbox.ru>
2023-05-16 11:16:34 +0300

    add JavaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Мир", phrase = "phrase", line = "line", text = "text";
+
+    }
+}
