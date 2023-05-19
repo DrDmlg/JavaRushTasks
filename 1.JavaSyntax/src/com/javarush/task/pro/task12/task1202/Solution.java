657823a1d0385e1c9185229ed18e81c0366ae867
dmitriy <b27011971@inbox.ru>
2023-05-19 13:10:28 +0300

    add JavaSyntax task1202

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1202/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task12.task1202;
+
+/* 
+Распаковка
+*/
+
+public class Solution {
+    Byte byteValueBox;
+    byte byteValue = byteValueBox.byteValue();
+    Short shortValueBox;
+    short shortValue = shortValueBox.shortValue();
+    Integer integerValueBox;
+    int intValue = integerValueBox.intValue();
+    Long longValueBox;
+    long longValue = longValueBox.longValue();
+
+    Float floatValueBox;
+    float floatValue = floatValueBox.floatValue();
+    Double doubleValueBox;
+    double doubleValue = doubleValueBox.doubleValue();
+
+    Character characterValueBox;
+    char charValue = characterValueBox.charValue();
+    Boolean booleanValueBox;
+    boolean booleanValue = booleanValueBox.booleanValue();
+
+    //напишите тут ваш код
+}
