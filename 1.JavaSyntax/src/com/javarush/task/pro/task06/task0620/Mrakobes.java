1fbdd064f4a66393e1c4ba19faa14734d6cddf32
dmitriy <b27011971@inbox.ru>
2023-05-30 17:17:27 +0300

    add JavaSyntax task0620

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0620/Mrakobes.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task06.task0620;
+
+public class Mrakobes {
+    private static boolean isAlive = true;
+    public static void setIsAlive(boolean isAliveParam) {
+        isAlive = isAliveParam;
+    }
+
+    public static void printState() {
+        System.out.println("Амиго" + (isAlive ? " НЕ " : " ") + "победил Мракобеса Безумного");
+    }
+}
