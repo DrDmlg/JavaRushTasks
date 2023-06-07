bb9c0353faf836c5c37dd892b586c5f2a9669605
dmitriy <b27011971@inbox.ru>
2023-06-07 21:39:58 +0300

    add JavaSyntax task0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int a){
+
+    }
+
+    public static void universalMethod(int a, int b){
+
+    }
+
+    public static void universalMethod(double a){
+
+    }
+
+    public static void universalMethod(double a, double b){
+
+    }
+
+    public static void universalMethod(char a, char b){
+
+    }
+
+    public static void universalMethod(String a, String b){
+
+    }
+
+    public static void universalMethod(boolean a){
+
+    }
+
+    public static void universalMethod(boolean a, boolean b){
+
+    }
+
+    public static void universalMethod(long a){
+
+    }
+}
+
