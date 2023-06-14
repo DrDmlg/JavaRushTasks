3103ef466ed9f4c5f0af8cf92f16aa5bff0f47a6
Dmitry Dorokhov <b27011971@inbox.ru>
2023-06-14 21:01:27 +0300

    add JavaSyntax task0614

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0614/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task06.task0614;
+
+/* 
+Вспоминаем геометрию
+*/
+
+public class Solution {
+    public static final double PI = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Площадь круга равна: " + PI * radius * radius);
+        System.out.println("Периметр круга равен: " + PI * diameter);
+    }
+}
+
