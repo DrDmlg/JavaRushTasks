0c2de302aed397f8ebf0c4ccb94f855435a3d311
Dmitry Dorokhov <b27011971@inbox.ru>
2023-06-16 20:06:05 +0300

    add JavaSyntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte a = Byte.MAX_VALUE;
+    short b = Short.MAX_VALUE;
+    int c = Integer.MAX_VALUE;
+    long d = Long.MAX_VALUE;
+}
