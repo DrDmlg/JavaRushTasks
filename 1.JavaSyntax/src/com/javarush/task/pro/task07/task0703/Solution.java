48c5f88421614c1cbd951a102faba725855894b2
Dmitry Dorokhov <b27011971@inbox.ru>
2023-06-17 22:06:40 +0300

    add JavaSyntax task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte a = Byte.MIN_VALUE;
+    short b = Short.MIN_VALUE;
+    int c = Integer.MIN_VALUE;
+    long d = Long.MIN_VALUE;
+}
