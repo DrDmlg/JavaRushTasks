700a7f855a313e20a037e9d17d52818b360abaa4
Dmitry Dorokhov <b27011971@inbox.ru>
2023-06-24 17:05:48 +0300

    add JavaSyntax task1218

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1218/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task12.task1218;
+
+import java.io.File;
+import java.util.*;
+
+/* 
+Shine bright like a diamond
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
