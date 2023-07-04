8766ff929def9d041eab535084a11e1ae7bd5791
Dmitry Dorokhov <b27011971@inbox.ru>
2023-07-04 22:31:45 +0300

    add JavaSyntax task1707

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Dog.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task17.task1707;
+
+public class Dog extends Pet {
+    public static final String DOG = "Я люблю людей.";
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println(DOG);
+    }
+
+    //напишите тут ваш код
+}
