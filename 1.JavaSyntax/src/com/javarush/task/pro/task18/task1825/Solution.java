4da549d1ef56c14c92a59fc6f810fc6cb9cbbcb3
Dmitry Dorokhov <b27011971@inbox.ru>
2023-07-19 14:41:08 +0300

    Add task1825

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1825/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task18.task1825;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
+/* 
+Из потока данных — в строку
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("To", "be", "a", "programmer", "you", "need", "to", "code");
+
+        System.out.println(getString(stringStream));
+    }
+
+    public static String getString(Stream<String> stringStream) {
+        //напишите тут ваш код
+        return stringStream.collect(Collectors.joining(" "));
+    }
+}
