8de6c05e8ec5900e15cb74f06ff55b21d6a511e4
Dmitry Dorokhov <b27011971@inbox.ru>
2023-07-30 21:48:08 +0300

    Add task0213

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0214/query.sql
@@ -0,0 +1,8 @@
+-- Write your code here:
+SELECT
+       CASE
+           WHEN experience < 1 THEN 'junior'
+           WHEN experience < 3 THEN 'middle'
+           WHEN experience < 5 THEN 'senior'
+END
+FROM developers;
