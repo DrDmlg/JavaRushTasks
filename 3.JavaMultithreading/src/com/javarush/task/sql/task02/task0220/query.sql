dd47f19e69e9a5efa935a92a83f5646afa9e4abb
Dmitry Dorokhov <b27011971@inbox.ru>
2023-07-30 22:02:40 +0300

    Add task0220

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0220/query.sql
@@ -0,0 +1,9 @@
+-- Write your code here:
+SELECT DISTINCT name,
+                CASE prod_year
+                    WHEN 2020 THEN 'new'
+                    WHEN 2021 THEN 'newer'
+                    WHEN 2022 THEN 'even newer'
+                    END
+FROM cars;
+
