cd1fc67b566075468fc04687aa3cbe4651b1b1f7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-07-31 15:40:03 +0300

    Add task MVC (task0368) stage3

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -5,10 +5,12 @@ package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
 import com.javarush.task.task36.task3608.model.ModelData;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
 
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -16,5 +18,10 @@ public class Controller {
 
     public void onShowAllUsers() {
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 }
