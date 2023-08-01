b7d8892c13f938beaa972f6387827859dbf3b33b
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-01 15:32:30 +0300

    Add task MVC (task0368) stage6

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -5,12 +5,19 @@ package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
 import com.javarush.task.task36.task3608.model.ModelData;
+import com.javarush.task.task36.task3608.view.EditUserView;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
 
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
+
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
+    }
 
     public void setModel(Model model) {
         this.model = model;
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
@@ -4,32 +4,19 @@ import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.controller.Controller;
 import com.javarush.task.task36.task3608.model.ModelData;
 
-import java.util.List;
-
-public class UsersView implements View {
+public class EditUserView implements View {
 
     private Controller controller;
 
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All " + (modelData.isDisplayDeletedUserList() ? "deleted " : "") + "users:");
-
-        List<User> users = modelData.getUsers();
-        //users.forEach(System.out::println);
-        users.forEach(user -> System.out.printf("\t%s%n", user));
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser());
         System.out.println("===================================================");
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
-    }
-
-    public void fireEventShowAllUsers() {
-        controller.onShowAllUsers();
-    }
-
-    public void fireEventShowDeletedUsers() {
-        controller.onShowAllDeletedUsers();
     }
 }
