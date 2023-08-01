23ff57c885458d6e017ace3dd5c4a34c4b2375e5
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-01 21:09:18 +0300

    Add task0244

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0244/query.sql
@@ -0,0 +1,6 @@
+-- Write your code here:
+SELECT YEAR(prod_date) AS prod_year, MONTH(prod_date) AS prod_month, COUNT(*)
+FROM cars
+WHERE name = 'Black Car' AND price > 99000
+GROUP BY prod_year, prod_month;
+
