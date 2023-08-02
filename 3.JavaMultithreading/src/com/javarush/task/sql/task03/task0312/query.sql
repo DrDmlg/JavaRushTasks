2f5aa130dce1a71759cb5d26c1fa55095100d822
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-02 22:57:04 +0300

    Add task0312

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0312/query.sql
@@ -0,0 +1,7 @@
+-- Write your code here:
+SELECT
+    top_retailers.rank AS company_rank,
+    top_retailers.name AS company_name,
+    suppliers.annual_revenue_billions AS supplier_revenue
+FROM top_retailers, suppliers
+WHERE suppliers.annual_revenue_billions > 25;
