aae3ecb7a0c2fa85dd98ffd7932d350f43042b5f
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-04 21:17:22 +0300

    Add task0330

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0330/query.sql
@@ -0,0 +1,7 @@
+-- Write your code here:
+SELECT
+    customers.zip_code AS cust_zip_code,
+    orders.order_status AS status
+FROM customers
+LEFT JOIN orders
+ON customers.customer_id = orders.customer_id;
