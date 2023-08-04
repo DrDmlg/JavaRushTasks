bb220f2a0a35eb9d7058d8399955c0c8bcb023e7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-04 21:16:05 +0300

    Add task0325

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0325/query.sql
@@ -0,0 +1,9 @@
+-- Write your code here:
+SELECT
+    c.zop_code,
+    o.order_id
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.customer_id = o.customer_id
+WHERE o.shipped_date IS NOT NULL
+AND o.store_id = 27;
