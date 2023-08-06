dc80f7db7be4c91a2a515f132db82d107a1e4194
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-06 21:48:32 +0300

    Add task0341

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0341/query.sql
@@ -0,0 +1,5 @@
+-- Write your code here:
+SELECT * FROM films
+WHERE title LIKE (SELECT title FROM films
+                               WHERE title LIKE 'The%r'
+                               LIMIT 1);
