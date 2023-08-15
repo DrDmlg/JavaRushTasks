c3e6968e5ff00c4cd8e7648b86142a50c0268dee
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-15 20:18:17 +0300

    Add task0505

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0505/query.sql
@@ -0,0 +1,6 @@
+-- Write your code here:
+INSERT INTO object(name, type, discoverer, discovery_date) VALUES
+                                                               ('2020 XL5', 'ASTEROID', 'Pan-STARRS 1', '2020-12-12'),
+                                                               ('Moon', 'SATELLITE', null, null),
+                                                               ('Jupiter', 'PLANET', null, null),
+                                                               ('C/1910 A1', 'COMET', 'Robert T. A. Innes', '1910-01-17');
