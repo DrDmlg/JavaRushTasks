0f52a4d427f23e7784eb24cd9be3582d6a92fbf8
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-15 20:19:01 +0300

    Add task0508

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0508/query.sql
@@ -0,0 +1,6 @@
+-- Write your code here:
+UPDATE author SET full_name = 'Clive Staples Lewis' WHERE full_name = 'C. S. Lewis';
+UPDATE author SET full_name = 'John Ronald Reuel Tolkien' WHERE full_name = 'J. R. R. Tolkien';
+UPDATE author SET full_name = 'Friedrich Wilhelm Nietzsche' WHERE full_name = 'Friedrich Nietzsche';
+UPDATE author SET full_name = 'Stephen Edwin King' WHERE full_name = 'Stephen King';
+UPDATE author SET full_name = 'Aldous Leonard Huxley' WHERE full_name = 'Aldous Huxley';
