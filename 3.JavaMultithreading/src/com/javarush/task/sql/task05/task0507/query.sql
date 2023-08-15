8f92c6cd235580fe3334a95190fff616bf72ff26
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-15 20:18:46 +0300

    Add task0507

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
@@ -0,0 +1,3 @@
+-- Write your code here:
+UPDATE part SET name = 'Technic, Steering Wheel Hub 3 Pin Round' WHERE number = 92909;
+UPDATE part SET name  = 'Minifigure, Headgear Helmet SW Darth Vader Type 2 Top' WHERE number = 19916;
