ebc1ebe48777cf5ee95e5c789edbf9bd83673d23
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-19 18:02:03 +0300

    Add task2510

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task25.task2510;
+
+/* 
+Поживем - увидим
+*/
+
+public class Solution extends Thread {
+
+    public Solution() {
+        this.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                if(e instanceof Error){
+                    System.out.println("Нельзя дальше работать");
+                } else if (e instanceof Exception){
+                    System.out.println("Надо обработать");
+                } else {
+                    System.out.println("Поживем - увидим");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void run() {
+        try {
+            throw new Error();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.start();
+    }
+}
