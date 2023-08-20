52770538f4de0fe152e72fa688d51ad81d908f30
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-20 19:23:52 +0300

    Add task0629

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0629/query.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `event`
+(
+    `id`      INT         not null auto_increment,
+    `date`    DATE        not null,
+    `user_id` INT         not null,
+    `type`    VARCHAR(20) not null,
+    `status`  VARCHAR(10) not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id)
+        REFERENCES user(id)
+
+
+);
+
+insert into event (id, date, user_id, type, status)
+values (1, '2022-10-01', 1, 'REGISTRATION', 'OK');
