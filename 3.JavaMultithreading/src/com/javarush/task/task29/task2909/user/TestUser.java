25f538605f9a11d5527f78a2b38220672ea3ca7e
Dmitry Dorokhov <b27011971@inbox.ru>
2023-08-28 15:55:16 +0300

    Add REFACTORING(task 2909) stage13

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/TestUser.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task29.task2909.user;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestUser {
+
+    public static void main(String[] args) {
+
+    }
+}
