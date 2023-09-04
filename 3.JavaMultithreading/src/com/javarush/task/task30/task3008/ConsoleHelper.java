fb691e2d4237a1e2077929a2beaa29b15a863249
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-04 16:05:20 +0300

    Add task CHAT(task2609) stage2

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,5 +1,37 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //вспомогательный класс, для чтения или записи в консоль.
 public class ConsoleHelper {
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                String input = reader.readLine();
+                if (input != null) return input;
+            } catch (IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString().trim());
+            } catch (NumberFormatException e) {
+                writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+    }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Test.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task30.task3008;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        String s = ConsoleHelper.readString();
+//        System.out.println(s);
+
+
+        int i = ConsoleHelper.readInt();
+        System.out.println(i);
+    }
+}
