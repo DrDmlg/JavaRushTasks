87631d5ebe43e5c3a7ae2793ca31cdaaf48fc07e
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-04 17:28:48 +0300

    Add task CHAT(task2609) stage5

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,5 +1,45 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 //Класс соединения между клиентом и сервером
-public class Connection {
+public class Connection implements Closeable {
+
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (this.out) {
+            out.writeObject(message);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (this.in) {
+            return (Message) in.readObject();
+        }
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
 }
