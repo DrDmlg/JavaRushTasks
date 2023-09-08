eb9ff66bbb8c6143be2c58a3eb20d3a88a8f4fe9
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-08 19:47:07 +0300

    Add task CHAT(task2609) stage8

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -6,14 +6,12 @@ import java.net.Socket;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-
 public class Server {
 
     private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера");
-
         try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
             ConsoleHelper.writeMessage("Сервер запущен");
             while (true) {
@@ -27,15 +25,14 @@ public class Server {
     }
 
     public static void sendBroadcastMessage(Message message) {
-        String data = message.getData();
-        for (String key : connectionMap.keySet()) {
-            Connection connection = connectionMap.get(key);
+
+        connectionMap.forEach((key, connection) -> {
             try {
                 connection.send(message);
             } catch (IOException e) {
                 ConsoleHelper.writeMessage("Возникла ошибка при отправке сообщения.");
             }
-        }
+        });
     }
 
 
@@ -49,7 +46,69 @@ public class Server {
 
         @Override
         public void run() {
+            try {
+                serverHandshake(new Connection(socket));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message message = connection.receive();
+
+                if (message.getType() != MessageType.USER_NAME) {
+                    continue;
+                }
+
+                if (message.getData().equals("")) {
+                    continue;
+                }
+
+                if (!connectionMap.containsKey(message.getData())) {
+                    connectionMap.put(message.getData(), connection);
+                    connection.send(new Message(MessageType.NAME_ACCEPTED));
+                    return message.getData();
+                }
+            }
         }
     }
 }
+
+
+
+/*
+ private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            String userName = "";
+            while (true) {
+                connection.send(new Message(MessageType.USER_NAME, "Введите имя пользователя"));
+                Message message = connection.receive();
+                if (message.getType() == MessageType.USER_NAME) {
+                    userName = message.getData();
+                    ConsoleHelper.writeMessage(userName + " Возможно имя пустое");
+                }
+                if ((!userName.equals("") && (!connectionMap.containsKey(userName)))) {
+                    connectionMap.put(userName, connection);
+                    connection.send(new Message(MessageType.USER_NAME, "Ваше имя принято!"));
+                }
+                return userName;
+            }
+        }
+    }
+}
+ */
+
+  /* for (String key : connectionMap.keySet()) {
+            Connection connection = connectionMap.get(key);
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Возникла ошибка при отправке сообщения.");
+            }
+        }
+
+         */
