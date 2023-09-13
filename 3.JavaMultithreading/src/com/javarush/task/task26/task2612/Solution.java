5d7df4a0a2625f47ddf95eace58ac77214616ab0
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-13 17:26:11 +0300

    Add task2612

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2612/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task26.task2612;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/* 
+Весь мир играет комедию
+*/
+
+public class Solution {
+    private Lock lock = new ReentrantLock();
+
+    public void someMethod() {
+        try {
+            if (lock.tryLock()) {
+                try {
+                    actionIfLockIsFree();
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                actionIfLockIsBusy();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    // Implement the logic here. Use the lock field
+
+
+    public void actionIfLockIsFree() {
+    }
+
+    public void actionIfLockIsBusy() {
+    }
+}
