ca69381307e80a554d5882a37bd215cac505a9ab
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-15 22:51:07 +0300

    Add task4310

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4310/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.jdk13.task43.task4310;
+
+import com.javarush.task.task30.task3008.Connection;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Какая операция 3
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Set<Integer> setA = Set.of(435, 12, 56, 1, 9066);
+        Set<Integer> setB = Set.of(1, 435, 12, 56, 11, 9067);
+        System.out.println(compute(setA, setB));
+    }
+
+    public static Collection<Integer> compute(Set<Integer> setA, Set<Integer> setB) {
+       return CollectionUtils.subtract(setA, setB);
+    }
+}
+
+//    Set<Integer> result = new HashSet<>();
+//        result.addAll(setA);
+//                result.removeAll(setB);
+//                return result;
