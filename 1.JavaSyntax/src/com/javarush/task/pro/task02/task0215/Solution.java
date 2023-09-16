1da0530658e36121c4e23993109ef3a0495555fd
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-16 11:03:13 +0300

    Add task0215

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        int c = console.nextInt();
+
+        System.out.println((a + b + c) / 3);
+
+    }
+}
