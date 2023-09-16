552aa4ed5674359771d9c7a9c957ce22417ceed2
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-16 11:02:07 +0300

    Add task0218

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0218;
+
+import java.util.Scanner;
+
+/* 
+Ручное управление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        int c = console.nextInt();
+        System.out.println(a * b * c);
+    }
+}
