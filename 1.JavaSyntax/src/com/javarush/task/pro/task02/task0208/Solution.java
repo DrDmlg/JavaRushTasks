3de24dcf1d1d558aac5150a2dec58915545185b3
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-16 10:57:59 +0300

    Add task0208

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0208/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0208;
+
+/* 
+Пустота и пробелы
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String emptiness = "пустота";
+
+        String fullness = emptiness + " " + emptiness + " " + emptiness;
+
+        System.out.println(fullness);
+    }
+}
