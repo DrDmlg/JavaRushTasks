5dd13438b8d563c46e230083431b8cd1e2b71c71
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-18 16:37:53 +0300

    Add task0302

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+/* 
+Призывная кампания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+
+        if(age >= 18 && age <= 28 ){
+            System.out.println(name + militaryCommissar);
+        }
+    }
+}
