739ddaf6085bfe37c8e30f58bd9f418b0758306b
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-18 16:46:36 +0300

    Add task0311

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task03.task0311;
+
+import java.util.Scanner;
+
+/* 
+Высокая точность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        if (Math.abs(a - b) < 0.000001) {
+            System.out.println("числа равны");
+        } else {
+            System.out.println("числа не равны");
+        }
+    }
+}
