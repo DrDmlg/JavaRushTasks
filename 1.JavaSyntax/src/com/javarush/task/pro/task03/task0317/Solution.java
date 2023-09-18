81e97c2e1a0264e7f1f6313cb97402e9a1f3f527
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-18 16:40:02 +0300

    Add task0317

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0317/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task03.task0317;
+
+import java.util.Scanner;
+
+/* 
+Пищевой конвеер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        if (a > 0) {
+            System.out.println(a);
+        }
+
+        if (b > 0) {
+            System.out.println(b);
+        }
+        if (c > 0) {
+            System.out.println(c);
+        }
+        if (d > 0) {
+            System.out.println(d);
+        }
+    }
+}
