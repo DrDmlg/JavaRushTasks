9b1527718db526efc7fd1066fdb355bc6f7eeedd
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-19 15:42:31 +0300

    Add task "Archiver" stage5

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task31.task3110.command;
+
+/**
+ * Класс реализующий команду добавления файла в архив
+ */
+public class ZipAddCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task31.task3110.command;
+
+/**
+ * Базовый класс для работы с архивом. От него будут наледоваться классы реализующие команды
+ */
+
+public abstract class ZipCommand implements Command {
+
+
+}
