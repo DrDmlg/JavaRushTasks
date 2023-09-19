6dede51e9754b15c24196cd85a7b5a436b5ae5f9
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-19 16:46:13 +0300

    Add task0425 stage2

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.task.pro.task04.task0425;
 
 /* 
-Диггер (1)
+Диггер (2)
 */
 
 public class Solution {
@@ -15,7 +15,13 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                System.out.print(land);
+                if (i == 9) {
+                    System.out.print(bug);
+                } else if (i == 0 && j == 3) {
+                    System.out.print(robot);
+                } else {
+                    System.out.print(land);
+                }
             }
             System.out.println();
         }
