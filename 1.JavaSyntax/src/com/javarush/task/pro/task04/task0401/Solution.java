f8d35990d4a421d66660ee4fed79b8c5cb2e1c4c
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-19 16:44:11 +0300

    Add task0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+
+
+        int n = 100;
+        while (n > 0) {
+            System.out.println(quote);
+            n--;
+        }
+
+    }
+}
