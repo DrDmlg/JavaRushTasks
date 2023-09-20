bf1dad26535d36df7ec718fda760b09f15cf02f7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-20 16:41:37 +0300

    Add task0419

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0419/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task04.task0419;
+
+import java.util.Scanner;
+
+/* 
+Защита от землетрясения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int sum = 0;
+        if (number <= 0) {
+
+        } else {
+            while (number >= 0) {
+                sum += number;
+                number--;
+            }
+        }
+        System.out.println(sum);
+    }
+}
