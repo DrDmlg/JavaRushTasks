f7cd3828bf3d4f230e8885a070f40591310d590f
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-20 09:28:02 +0300

    Add task "Archiver" stage7

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/PathIsNotFoundException.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task31.task3110.exception;
+
+/**
+ * Исключение PathIsNotFoundException будем кидать, если не сможем найти путь, в который нужно распаковать архив,
+ * или путь к файлу, который хотим запаковать, или любой другой
+ */
+public class PathIsNotFoundException extends Exception {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/WrongZipFileException.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task31.task3110.exception;
+
+/**
+ * Исключение WrongZipFileException будем кидать, если будет попытка сделать что-нибудь с архивом, который не существует
+ */
+public class WrongZipFileException extends Exception {
+}
