a9388395b665237d9a8ca443f101ef938d0a44e0
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-20 16:39:49 +0300

    Add task0405

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task04.task0405;
+
+/* 
+Незаполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int height = 1;
+        while (height <= 10) {
+            int width = 1;
+            while (width <= 20) {
+                if (height == 1 || height == 10) {
+                    System.out.print('Б');
+                } else if (width == 1 || width == 20) {
+                    System.out.print('Б');
+                } else {
+                    System.out.print(' ');
+                }
+                width++;
+            }
+            System.out.println();
+            height++;
+        }
+    }
+}
