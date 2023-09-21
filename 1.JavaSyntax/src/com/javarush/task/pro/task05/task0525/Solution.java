c802deb6aeadb9df0200baf17faa942fb8a1b221
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-21 17:06:35 +0300

    Add task0525

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0525/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task05.task0525;
+
+/* 
+Ловушка для Роботанка
+*/
+
+public class Solution {
+    public static String[] array = {"Amigo", "Diego", "robotank", "Amigo", "Diego","Amigo", "Diego"};
+
+    public static void main(String[] args) {
+
+        boolean isMissionComplete = false;
+        for (int i = 0; i < array.length; i++) {
+            if ("robotank".equalsIgnoreCase(array[i])) {
+                isMissionComplete = true;
+                break;
+            }
+        }
+        System.out.println(isMissionComplete ? "Mission complete" : "Mission not completed");
+    }
+}
