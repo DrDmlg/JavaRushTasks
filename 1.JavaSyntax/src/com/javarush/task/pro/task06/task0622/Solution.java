c700dfe73a1ea2ef51de98451f68b10bd61c0d8b
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-22 17:08:19 +0300

    Add task0622 stage2

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
@@ -7,36 +7,54 @@ import java.util.Scanner;
 */
 
 public class Solution {
-        public static String getFirstPositionPhrase = "Помоги Амиго определить, где спрятался Диабло? Введи номер позиции (1, 2, 3 или 4):";
-        public static String getPositionPhrase = "Диабло не обнаружен. Давай попробуем снова. Введи номер позиции (1, 2, 3 или 4):";
-        public static String findDiabloPhrase = "Амиго обнаружил Диабло.";
-        public static String diabloDefendPhrase = "Амиго атакует. Диабло отбил атаку Амиго и нанес ответный удар.";
-        public static String amigoAttackPhrase = "Удар Амиго достиг цели. Диабло потерял 3 жизни.";
-        public static String winPhrase = "Амиго одержал победу над Диабло и выбил из его головы загадочный кристалл.";
-        public static String loosePhrase = "Диабло победил Амиго.";
-        public static int diabloPosition;
+    public static String getFirstPositionPhrase = "Помоги Амиго определить, где спрятался Диабло? Введи номер позиции (1, 2, 3 или 4):";
+    public static String getPositionPhrase = "Диабло не обнаружен. Давай попробуем снова. Введи номер позиции (1, 2, 3 или 4):";
+    public static String findDiabloPhrase = "Амиго обнаружил Диабло.";
+    public static String diabloDefendPhrase = "Амиго атакует. Диабло отбил атаку Амиго и нанес ответный удар.";
+    public static String amigoAttackPhrase = "Удар Амиго достиг цели. Диабло потерял 3 жизни.";
+    public static String winPhrase = "Амиго одержал победу над Диабло и выбил из его головы загадочный кристалл.";
+    public static String loosePhrase = "Диабло победил Амиго.";
+    public static int diabloPosition;
+    public static int amigoLives = 9;
+    public static int diabloLives = 9;
 
-        public static void main(String[] args) {
-            diabloPosition = getRandomNumber(4);
+    public static void main(String[] args) {
+        diabloPosition = getRandomNumber(4);
 
-            findDiablo();
-        }
+        findDiablo();
+    }
 
-        public static void findDiablo() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(getFirstPositionPhrase);
-            while (true) {
-                int position = scanner.nextInt();
-                if (position == diabloPosition) {
-                    System.out.println(findDiabloPhrase);
-                    break;
-                } else {
-                    System.out.println(getPositionPhrase);
-                }
+    public static void findDiablo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getFirstPositionPhrase);
+        while (true) {
+            int position = scanner.nextInt();
+            if (position == diabloPosition) {
+                System.out.println(findDiabloPhrase);
+                break;
+            } else {
+                System.out.println(getPositionPhrase);
             }
         }
-
-        public static int getRandomNumber(int range) {
-            return (int) (Math.random() * range) + 1;
-        }
     }
+
+    public static void amigoLostLife() {
+        amigoLives--;
+    }
+
+    public static void diabloLostLife() {
+        diabloLives -= 3;
+    }
+
+    public static int amigoAttacks() {
+        return getRandomNumber(3);
+    }
+
+    public static int diabloDefends() {
+        return getRandomNumber(3);
+    }
+
+    public static int getRandomNumber(int range) {
+        return (int) (Math.random() * range) + 1;
+    }
+}
