72b5498e7e27cf08515c56211eeb05ee7ad138b5
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-25 16:38:42 +0300

    Add task0720

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/Nimrod.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0720;
+
+public class Nimrod {
+    public static int laser = 100_000;
+    public static int rocket = 10_000;
+    public static int angelicaKiss;
+
+    public int health = 1_000_000;
+
+    public void defend(int damage) {
+        health -= damage;
+    }
+
+    public int attack() {
+        return rocket;
+    }
+}
