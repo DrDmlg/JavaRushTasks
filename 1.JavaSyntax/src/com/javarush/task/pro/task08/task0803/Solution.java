f53760ee13db1b203f85657aca81453216cb4c23
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-25 17:19:25 +0300

    Add task0803

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0803/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task08.task0803;
+
+import java.util.Scanner;
+
+/* 
+Минимальный элемент массива
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] intArray = getArrayOfTenElements();
+        System.out.println(min(intArray));
+    }
+
+    public static int min(int[] ints) {
+        int min = ints[0];
+        for (int number : ints) {
+            min = Math.min(min, number);
+        }
+        return min;
+    }
+
+    public static int[] getArrayOfTenElements() {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+}
