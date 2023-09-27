54ffc2bc385b30b5c19fb2da82d0ccbfc0132d95
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-27 17:42:24 +0300

    Add task1020

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1020;
+
+import java.security.Key;
+
+public class Computer {
+
+    public Computer() {
+        new SystemUnit();
+        new Keyboard();
+        new Monitor();
+        new Mouse();
+    }
+
+}
