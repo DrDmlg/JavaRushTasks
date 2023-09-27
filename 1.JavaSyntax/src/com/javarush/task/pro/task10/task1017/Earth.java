7bc932d4f8cf0ab4f3e0eb9a4dabb5d411da5dd3
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-27 17:41:34 +0300

    Add task1017

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        new Africa(30_370_000);
+        new Antarctica(14_200_000);
+        new Australia(8_600_000);
+        new Eurasia(55_000_000);
+        new NorthAmerica(24_709_000);
+        new SouthAmerica(17_840_000);
+    }
+}
