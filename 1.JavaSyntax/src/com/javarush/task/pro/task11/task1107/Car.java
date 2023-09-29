192593836072104f4f3b5d8864398ce2ae9225a7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-29 17:31:02 +0300

    Add task1107

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task11.task1107;
+
+/* 
+Двигатель — сердце автомобиля
+*/
+
+public class Car {
+
+    Engine engine;
+    class Engine {
+       private boolean isRunning;
+
+        public void start(){
+            isRunning = true;
+        }
+
+        public void stop(){
+            isRunning = false;
+        }
+
+    }
+}
