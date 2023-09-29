d0bb2378e766de6967d7d6005a18bb3b4068a89e
Dmitry Dorokhov <b27011971@inbox.ru>
2023-09-29 09:32:09 +0300

    Add task2812

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2812/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task28.task2812;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/* 
+ShutdownNow!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 1; i <= 10; i++) {
+            final int localId = i;
+            executor.submit(new Runnable() {
+                public void run() {
+                    doExpensiveOperation(localId);
+                }
+            });
+        }
+
+        executor.shutdownNow().forEach(System.out::println);
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    }
+}
