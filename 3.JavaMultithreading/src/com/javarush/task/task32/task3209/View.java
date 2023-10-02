0efde5939da37d62cd278b0c7510018205e66709
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-02 16:20:29 +0300

    Add task "HTML editor" stage3

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -8,6 +8,11 @@ public class View extends JFrame implements ActionListener {
 
     private Controller controller;
 
+    private JTabbedPane tabbedPane = new JTabbedPane(); // панель с двумя вкладками
+    private JTextPane htmlTextPane = new JTextPane(); // компонент для визуального редактирования html(на первой вкладке)
+    private JEditorPane plainTextPane = new JEditorPane(); // компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое).
+
+
     public Controller getController() {
         return controller;
     }
