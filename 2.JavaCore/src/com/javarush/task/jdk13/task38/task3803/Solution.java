8e47fdaac86d8c1345da965615c3bd75f8049f44
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-02 14:15:44 +0300

    Add task3803

--- 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.task.jdk13.task38.task3803;
 Обработка аннотаций
 */
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         printFullyQualifiedNames(Solution.class);
@@ -14,10 +16,20 @@ public class Solution {
     }
 
     public static <T> boolean printFullyQualifiedNames(Class<T> c) {
-        return true;
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            Arrays.stream(c.getAnnotation(PrepareMyTest.class).fullyQualifiedNames())
+                            .forEach(name -> System.out.println("Array fullyQualifiedNames: " + name));
+            return true;
+        }
+        return false;
     }
 
     public static <T> boolean printValues(Class<T> c) {
-        return true;
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            Arrays.stream(c.getAnnotation(PrepareMyTest.class).value())
+                    .forEach(name -> System.out.println("Array value: " + name.getSimpleName()));
+            return true;
+        }
+        return false;
     }
 }
