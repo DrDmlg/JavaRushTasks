a6aa442ce237d77a1c2254aceaef1ab012ac9be5
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-03 15:45:18 +0300

    Add task "HTML editor" stage9

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -4,26 +4,31 @@ import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
-
     private Controller controller;
 
-    private JTabbedPane tabbedPane = new JTabbedPane(); // панель с двумя вкладками
-    private JTextPane htmlTextPane = new JTextPane(); // компонент для визуального редактирования html(на первой вкладке)
-    private JEditorPane plainTextPane = new JEditorPane(); // компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое).
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
 
-
-    public Controller getController() {
-        return controller;
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
     }
 
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public Controller getController() {
+        return controller;
     }
 
     @Override
@@ -33,28 +38,39 @@ public class View extends JFrame implements ActionListener {
 
     public void init() {
         initGui();
-        FrameListener listener = new FrameListener(this);
-        addWindowListener(listener);
+        addWindowListener(new FrameListener(this));
         setVisible(true);
     }
 
-    public void initMenuBar() {
+    public void exit() {
+        controller.exit();
+    }
 
+    public void initMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        MenuHelper.initFileMenu(this, menuBar);
+        MenuHelper.initEditMenu(this, menuBar);
+        MenuHelper.initStyleMenu(this, menuBar);
+        MenuHelper.initAlignMenu(this, menuBar);
+        MenuHelper.initColorMenu(this, menuBar);
+        MenuHelper.initFontMenu(this, menuBar);
+        MenuHelper.initHelpMenu(this, menuBar);
+
+        getContentPane().add(menuBar, BorderLayout.NORTH);
     }
 
     public void initEditor() {
         htmlTextPane.setContentType("text/html");
+        JScrollPane htmlScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", htmlScrollPane);
 
-        JScrollPane htmljScrollPane = new JScrollPane(htmlTextPane);
-        tabbedPane.add("HTML", htmljScrollPane);
+        JScrollPane plainScrollPane = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", plainScrollPane);
 
-        JScrollPane plainjScrollPane = new JScrollPane(plainTextPane);
-        tabbedPane.add("Текст", plainjScrollPane);
+        tabbedPane.setPreferredSize(new Dimension(300, 300));
 
-        tabbedPane.setPreferredSize(new Dimension(200, 200));
-
-        TabbedPaneChangeListener listener = new TabbedPaneChangeListener(this);
-        tabbedPane.addChangeListener(listener);
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
 
         getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
@@ -67,9 +83,5 @@ public class View extends JFrame implements ActionListener {
 
     public void selectedTabChanged() {
 
-    }
-
-    public void exit() {
-        controller.exit();
     }
 }
