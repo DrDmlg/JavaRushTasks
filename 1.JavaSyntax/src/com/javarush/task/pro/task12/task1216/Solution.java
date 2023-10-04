d3097d7dc9a8f6dd3516dd74ecd51ef8a107caa3
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-04 16:50:31 +0300

    Add task1216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1216/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task12.task1216;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вперед в будущее
+*/
+
+public class Solution {
+    private int number = 54;
+    private String string = "string";
+
+    public static void main(String[] args) {
+        int integer = 22;
+        String string = "string";
+        int[] array = new int[5];
+        ArrayList<String> strings = new ArrayList<String>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Integer maxValue = Integer.valueOf(Integer.MAX_VALUE);
+    }
+}
