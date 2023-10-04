ca590cb8960b8a580a6473728a84a6e4b80fd5d7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-04 16:53:15 +0300

    Add task1218

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1218/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1218/Solution.java
@@ -10,6 +10,10 @@ Shine bright like a diamond
 public class Solution {
 
     public static void main(String[] args) {
-
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
     }
 }
