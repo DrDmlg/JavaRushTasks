75e2665d2eab638b49676e308e0f59c8469ea06f
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-05 18:40:59 +0300

    Add task1318

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1318/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task13.task1318;
+
+/* 
+Следующий месяц, пожалуйста
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getNextMonth(Month.JANUARY));
+        System.out.println(getNextMonth(Month.JULY));
+
+    }
+
+    public static Month getNextMonth(Month month) {
+        int index = month.ordinal();
+        if (index < 11) {
+            return Month.values()[index + 1];
+        } else {
+            return Month.JANUARY;
+        }
+    }
+}
