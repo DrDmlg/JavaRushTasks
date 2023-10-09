b3d100633b3b134180b29856e474483b14ae4c20
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-09 15:41:48 +0300

    Add task3502

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3502/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task35.task3502;
+
+import java.util.*;
+
+/* 
+Знакомство с дженериками
+*/
+
+public class Solution<T extends List<Solution.SomeClass<?>>> {
+    public static void main(String[] args) {
+
+    }
+
+    public static class SomeClass<T extends Number> {
+
+    }
+}
+
+
