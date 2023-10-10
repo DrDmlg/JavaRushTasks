4089c2f49072b0514c00758538928fe682635bd8
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-10 20:23:42 +0300

    Add task1709

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактные фигуры
+*/
+
+abstract class Shape {
+    
+    abstract double getPerimeter();
+
+    abstract double getArea();
+
+}
