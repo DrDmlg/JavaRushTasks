6c3200105888f6f021bb5252660636ae0f5c7209
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-10 20:20:22 +0300

    Add task1523

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1523/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task15.task1523;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+/* 
+Получение информации по API
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("http://httpbin.org/post");
+        URLConnection connection = url.openConnection();
+        connection.setDoOutput(true);
+
+        try (OutputStream output = connection.getOutputStream();
+             PrintStream sender = new PrintStream(output)) {
+            sender.println("Привет");
+        }
+
+        try (InputStream input = connection.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+            while (reader.ready()) {
+                System.out.println(reader.readLine());
+            }
+        }
+    }
+}
