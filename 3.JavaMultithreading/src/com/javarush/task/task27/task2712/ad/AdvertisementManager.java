fdc4025da94b1c48f56e2e601bcb4b7ded41643b
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-11 15:50:48 +0300

    Add task "Restaurant" stage7

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -2,4 +2,6 @@ package com.javarush.task.task27.task2712.ad;
 
 public class AdvertisementManager {
 
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,12 +1,19 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class AdvertisementStorage { //хранилище рекламных роликов
 
     private static AdvertisementStorage INSTANCE;
+    private final List<Advertisement> videos = new ArrayList<>();
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));  //10 min
     }
 
     public static AdvertisementStorage getInstance() {
@@ -14,5 +21,13 @@ public class AdvertisementStorage { //хранилище рекламных ро
             INSTANCE = new AdvertisementStorage();
         }
         return INSTANCE;
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 }
