f48c4ec97462f1c4df07dbe36571995d2dca5bd2
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-12 22:02:40 +0300

    Add task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(5);
+        print((Integer)4);
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(Integer a) {
+
+    }
+
+}
