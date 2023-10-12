5aa197632f8652537a9137bf15f7155e31269556
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-12 22:01:55 +0300

    Add task1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String a) {
+
+    }
+
+    public static void print(int b) {
+
+    }
+}
