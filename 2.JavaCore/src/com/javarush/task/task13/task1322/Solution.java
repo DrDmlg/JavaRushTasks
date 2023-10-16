33767270dd36ab5f015760b85492650ea593f135
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-16 22:26:05 +0300

    Add task1322

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1322/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task13.task1322;
+
+/* 
+Интерфейс SimpleObject
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        SimpleObject<String> stringObject = new StringObject();
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+
+    static class StringObject implements SimpleObject<String> {
+        @Override
+        public StringObject getInstance() {
+            return new StringObject();
+        }
+    }
+
+}
