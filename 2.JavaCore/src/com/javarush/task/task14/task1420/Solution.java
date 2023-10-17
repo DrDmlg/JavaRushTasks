7de603f387419cac493bb6e1de3e79f2fac3be47
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-17 22:26:25 +0300

    Add task1420

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task14.task1420;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+/* 
+НОД
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger bigInteger = new BigInteger(reader.readLine());
+        BigInteger bigInteger2 = new BigInteger(reader.readLine());
+
+        System.out.println(bigInteger.gcd(bigInteger2));
+    }
+    public static void main1(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+
+
+        for (int i = Math.min(num1, num2); i > 0; i--) {
+            if(num1 % i == 0 && num2 % i == 0){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
