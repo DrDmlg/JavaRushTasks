c0dec96d0e5f734a78e144575725b7619f571505
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-18 20:38:39 +0300

    Add task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution() {
+
+    }
+
+    private Solution(String s) {
+
+    }
+
+    Solution(Integer s) {
+
+    }
+
+    protected Solution(Short s) {
+
+    }
+
+
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    public SubSolution() {
+    }
+
+    SubSolution(Integer s) {
+        super(s);
+    }
+
+    protected SubSolution(Short s) {
+        super(s);
+    }
+}
