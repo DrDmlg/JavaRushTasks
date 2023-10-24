c97a865317d6fd0697ddc3f547a5c77b413de6e3
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-24 15:00:42 +0300

    Add task3506

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3506/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task35.task3506;
+
+import java.util.List;
+
+/* 
+extends vs super
+*/
+
+public abstract class Solution {
+    public abstract <T> void one(List<T> destination, List<T> source);
+
+    public abstract <T> void two(List<T> destination, List<? extends T> source);
+
+    public abstract <T> void three(List<? super T> destination, List<T> source);
+
+    public abstract <T> void four(List<? super T> destination, List<? extends T> source);
+
+}
+
