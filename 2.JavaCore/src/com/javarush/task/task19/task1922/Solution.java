5654a623fdeda49d40d1bfb58abeef004c8136e6
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-25 18:24:03 +0300

    Add task1922

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task19.task1922;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+
+/* 
+Ищем нужные строки
+*/
+
+public class Solution {
+    public static List<String> words = new ArrayList<String>();
+
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String file1 = null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            file1 = reader.readLine();
+
+            try (BufferedReader in = new BufferedReader(new FileReader(file1))) {
+                while (in.ready()) {
+                    String readedString = in.readLine();
+                    String[] splitedReadedString = readedString.split(" ");
+
+                    int wordCount = 0;
+                    for (String s : splitedReadedString) {
+                        if (words.contains(s)) {
+                            wordCount++;
+                        }
+                    }
+
+                    if (wordCount == 2)
+                        System.out.println(readedString);
+                }
+            }
+        }
+    }
+}
