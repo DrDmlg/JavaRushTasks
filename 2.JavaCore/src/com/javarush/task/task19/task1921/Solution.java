81349b0a991e57f94045c52b1fd7c3808a0cc7b7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-25 18:23:28 +0300

    Add task1921

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task19.task1921;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/* 
+Хуан Хуанович
+*/
+
+public class Solution {
+    public static final List<Person> PEOPLE = new ArrayList<Person>();
+
+    public static void main(String[] args) throws IOException, ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MM yyyy");
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                String name = line.replaceAll("\\d+", " ").trim();
+                String date = line.replaceAll("\\D+", " ").trim();
+               PEOPLE.add(new Person(name, simpleDateFormat.parse(date)));
+            }
+        }
+    }
+}
