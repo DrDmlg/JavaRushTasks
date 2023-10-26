be25cc6f9cc794bbc2c3e9bdbd0afe7a28ee4767
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-26 19:50:47 +0300

    Add task2013

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2013/Solution.java
@@ -0,0 +1,88 @@
+package com.javarush.task.task20.task2013;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.List;
+
+/* 
+Externalizable Person
+*/
+
+public class Solution {
+    public static class Person implements Externalizable {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private Person mother;
+        private Person father;
+        private List<Person> children;
+
+        public Person(String firstName, String lastName, int age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public Person() {
+
+        }
+
+        public void setMother(Person mother) {
+            this.mother = mother;
+        }
+
+        public void setFather(Person father) {
+            this.father = father;
+        }
+
+        public void setChildren(List<Person> children) {
+            this.children = children;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(firstName);
+            out.writeObject(lastName);
+            out.writeInt(age);
+            out.writeObject(mother);
+            out.writeObject(father);
+            out.writeObject(children);
+
+
+           /* out.writeObject(mother);
+            out.writeObject(father);
+            out.writeChars(firstName);
+            out.writeChars(lastName);
+            out.writeInt(age);
+            out.writeObject(children);
+
+            */
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            firstName = (String) in.readObject();
+            lastName = (String) in.readObject();
+            age = in.readInt();
+            mother = (Person) in.readObject();
+            father = (Person) in.readObject();
+            children = (List<Person>) in.readObject();
+
+
+            /*firstName = in.readLine();
+            lastName = in.readLine();
+            father = (Person) in.readObject();
+            mother = (Person) in.readObject();
+            age = in.readInt();
+            children = (List) in.readObject();
+
+             */
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
