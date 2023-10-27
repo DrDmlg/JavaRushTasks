e8583f0c9c3daadbc9918e3c10678509830af8c7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-27 20:58:23 +0300

    Add task2113 stage3

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
@@ -8,6 +9,10 @@ public class Hippodrome {
 
     public static void main(String[] args) {
 
+    }
+
+    public Hippodrome(List<Horse> horses){
+        this.horses = horses;
     }
 
     public List<Horse> getHorses() {
