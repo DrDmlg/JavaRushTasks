a903fba29e5e5bc4f0ad8eb5dc742bb6e2b9dce4
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-27 21:03:01 +0300

    Add task2113 stage10

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -39,7 +39,13 @@ public class Hippodrome {
     }
 
     public void print() {
+        for (Horse horse : horses) {
+            horse.print();
+        }
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
     }
 
 }
