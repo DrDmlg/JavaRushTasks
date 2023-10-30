0130f8c301d99d8cd8612149c29f7f7828d64d01
Dmitry Dorokhov <b27011971@inbox.ru>
2023-10-30 20:50:19 +0300

    Add task2203

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -5,21 +5,20 @@ package com.javarush.task.task22.task2203;
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
-    }
-
     public static String getPartOfString(String string) throws TooShortStringException {
-        if (string == null) {
+        if(string == null){
             throw new TooShortStringException();
         }
-        String[] splitWithTabs = string.split("\\t");
+        String[] split = string.split("\\t");
+        if(split.length < 3) throw new TooShortStringException();
 
-        if (splitWithTabs.length < 3) throw new TooShortStringException();
-
-        return splitWithTabs[1];
+       return split[1];
     }
 
     public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
     }
 }
